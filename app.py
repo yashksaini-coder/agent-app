3872d6be2e4583ddee2325f7c03dbc13ba7a7763
yashksaini-coder <ys3853428@gmail.com>
2025-03-17 22:12:15 +0530

    chore: update .gitignore and enhance agent configurations

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import groq
 import os
 from dotenv import load_dotenv
+load_dotenv()
 
 # AI assistant imports
 from agno.agent import Agent
@@ -19,22 +20,21 @@ if not GROQ_API_KEY:
 web_search_agent = Agent(
     name="web_agent",
     role="search the web for information based on the user given input",
-    model=Groq(id="deepseek-r1-distill-llama-70b",api_key=GROQ_API_KEY),
+    model=Groq(id="llama-3.1-8b-instant",api_key=GROQ_API_KEY),
     tools=[
         DuckDuckGoTools(search=True, news=True),
 
     ],
     instructions=[
-        "You are a very professional web search AI agent",
-        "your job is to search the web for information based on the user given input",
-        "provide exact information to the user available on the web",
+        "Your job is to search and scrape the web for information based on the user queries",
+        "your main objective is to provide exact information to the user available on the web",
     ]
 )
 
 financial_agent = Agent(
     name="financial_agent",
     role="get financial information",
-    model=Groq(id="deepseek-r1-distill-llama-70b",api_key=GROQ_API_KEY),
+    model=Groq(id="llama-3.1-8b-instant",api_key=GROQ_API_KEY),
     tools=[
         YFinanceTools(stock_price=True,
                     analyst_recommendations=True,
@@ -47,10 +47,10 @@ financial_agent = Agent(
                     ),
     ],
     instructions=[
-        "You are a very professional financial advisor AI agent",
+        "You are a professional financial advisor AI agent",
         "your job is to provide financial information to users",
         "you can provide stock price, analyst recommendations, and stock fundamentals",
-        "you can also provide information about companies, industries, and financial terms",
+        "Provide information about companies, industries, and financial terms",
     ]
 )
 
@@ -59,3 +59,6 @@ multi_ai = Agent(
     model=Groq(id="deepseek-r1-distill-llama-70b",api_key=GROQ_API_KEY),
     markdown=True,
 )
+
+
+multi_ai.print_response("Top news of 2024 till today with proper links to them ?",stream=True)
